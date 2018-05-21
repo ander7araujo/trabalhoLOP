7a29a31e4fdf663be91f987431e4f643d603a112
ander7araujo <39493490+ander7araujo@users.noreply.github.com>
2018-05-21 12:48:11 -0300

    Create etapa1.js

---
+++ etapa1.js
@@ -0,0 +1,21 @@
+// Mova seu mouse na janela ao lado
+// para ver o processing em ação
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  fill(255);
+  rect(0, 0, 400, 400);
+
+  fill(200, 200, 200);
+  rect(mouseX - 40, mouseY - 40, 80, 80);
+}
+
+function draw() {
+  ellipse(300, 50, 100, 100);
+  triangle(40, 90, 90, 90, 90, 0);
+  
+}
+
