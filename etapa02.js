3e87a8cf7b0aef0983f01a454fa4ee589c3cf650
ander7araujo <39493490+ander7araujo@users.noreply.github.com>
2018-06-24 22:17:12 -0300

    Create etapa02.js

---
+++ etapa02.js
@@ -0,0 +1,22 @@
+function setup(){
+		createCanvas(1200, 600);
+		background(70);
+}
+
+function draw(){
+	ellipse(40,530,100, 120);
+	fill(0,0,0);
+	stroke(800,600,90);
+	rect(300, 450, 150,400);
+	fill(900,800,600);
+	stroke(100, 80, 100);
+	strokeWeight(4);
+	ellipse(150,450,100,120);
+	ellipse(230,390,100,120);
+	ellipse(330,370,100,120);
+	ellipse(430,370,100,120);
+	ellipse(530,430,100,120);
+	ellipse(600,530,100,120);
+	ellipse(900,530,100, 120)
+	stroke(30,90,20);
+	}
